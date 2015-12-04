c5904e5b377c8ff9543670b7a78f73ba1a12b9a0
Raul Piraces <raul.piraces@gmail.com>
2015-12-04 23:47:26 +0100

    Extended the query interface and .gitignore modified.

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +26,27 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        // Attempt to split the query string with keywords and count
+        String[] splitted = q.split("max:");
+        // Creation of query headers
+        Map<String,Object> headers = new HashMap<String,Object>();
+        if(splitted.length>1) {
+            // If max count is specified, then the max count is requested to the Twitter API
+            String message = splitted[0].trim();
+            try {
+                // Converts the count from String to Integer
+                Integer count = new Integer(splitted[1].trim());
+                headers.put("CamelTwitterKeywords",message);
+                headers.put("CamelTwitterCount",count);
+            } catch(NumberFormatException ex){
+                // If max count is not a number, only keywords go in the request
+                System.out.println("Warning: not a number specified for twitter counts");
+                headers.put("CamelTwitterKeywords",message);
+            }
+        } else {
+            // If there is no max count specified, only keywords go in the request
+            headers.put("CamelTwitterKeywords",q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
